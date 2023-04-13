787aaa4bfccff37109a25da5d9926e0cd20b3c04
igor110890 <lunevigor110890@yandex.ru>
2023-04-13 21:04:34 +0300

    Liquibase, dependency, build succes

---
+++ src/main/java/com/example/zooResources/ZooResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.example.zooResources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooResourcesApplication.class, args);
+	}
+
+}
